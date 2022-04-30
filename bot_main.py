9c8763db7458b4b4ebf0d4dabb5927075c897dd7
Gorn <daniilhorn8@gmail.com>
2022-04-30 22:16:21 +0300

    message reply error

--- bot_main.py
+++ bot_main.py
@@ -4,8 +4,6 @@ import webbrowser
 import re
 import logging
 from time import time, sleep
-
-
 from config import *
 import command_os
 
@@ -14,45 +12,59 @@ logger = logging.getLogger("tg-bot")
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-
 bot = telebot.TeleBot(key)
 
-"""
-смысл таков
-1 - отправляешь по кнопка команды
-2 - бот обрабатывает текст
-3 - выполняет команду из других модулей
-"""
+
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+# website = types.KeyboardButton('google')
+# linux_bot = types.KeyboardButton('shutdown')
+# markup.add(linux_bot)
 
 
-# метод для основных кнопок (основное меню)
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    hellper = """
+    instruction
+    buttons:
+        google - открывает гугл
+        shutdown - выкл пк
+    """
+
+    bot.send_message(message.chat.id, hellper, reply_markup=markup)
+
+
+# метод для команд (основное меню)
 @bot.message_handler(commands=['start'])
-def start(message): 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    website = types.KeyboardButton('site')
+def menu(message): 
     linux_bot = types.KeyboardButton('shutdown')
-    markup.add(website, linux_bot)
-    
-    bot.send_message(message.chat.id, 'Да, сэр', reply_markup=markup)
+
+
+    markup.add(linux_bot)
+    command_os.voice()
+
+    bot.send_message(message.chat.id, 'Здравствуйте, сэр', reply_markup=markup)
 
 
 # метод для обработки текста, после кнопок
 @bot.message_handler()
-def start_keyboard(message):
+def menu_responce(message):
 
     # открывает гугл
-    if message.text == "site":
+    # if message.text 
+    # google_searsh = re.finditer("загугли ", message.text)
+    if re.finditer("загугли ", message.text):
+        search = ' '.join(message.text.split()[1:])
+        url = f"https://www.google.com/search?q={search}"  # ссылка для поиска чего либо
+        webbrowser.open(url, new=0, autoraise=True)
+        # command_os.voice()
 
-        url = "https://www.google.linux.voice()com/search?q="  # ссылка для поиска чего либо
-        # webbrowser.open(url, new=0, autoraise=True)
-        command_os.voice()
-
-        bot.send_message(message.chat.id, 'open google')
+        bot.send_message(message.chat.id, f'гуглю: {search}')
 
     # поиск по указанной ссылке
-    link = re.search("https?://[\w.-]+", message.text)
+    # open_link = re.search("https?://[\w.-]+", message.text)
 
-    if link:
+    if re.search("https?://[\w.-]+", message.text):
         url = message.text  # ссылка для поиска чего либо
         webbrowser.open(url, new=0, autoraise=True)
         bot.send_message(message.chat.id, f'open: {message.text}')
@@ -65,16 +77,17 @@ def start_keyboard(message):
         bot.send_message(message.chat.id, 'вы уверены?', reply_markup=markup)
 
     if message.text == "yep":
+        
+        bot.send_message(message.chat.id, reply_markup=markup)
+        # menu(message)
+        # command_os.kill_and_shutdown()
 
-        start(message)
-        command_os.kill_and_shutdown()
 
 
-# @bot.message_handler(commands=['voice'])
-# def start(message):
 
-#     linux.voice()
-#     bot.send_message(message.chat.id, 'Да, сэр')
+
+
+
 
 
 bot.polling(none_stop=True)
