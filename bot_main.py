1553830f471895ec3e293042f0349c3dbe0d9864
Gorn <daniilhorn8@gmail.com>
2022-05-01 00:59:09 +0300

    fixed bugs got buttons

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,10 @@
-
 from telebot import *
-import webbrowser
 import re
 import logging
-from time import time, sleep
+
 from config import *
 import command_os
+import command_google
 
 
 logger = logging.getLogger("tg-bot")
@@ -15,59 +14,55 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 bot = telebot.TeleBot(key)
 
 
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-# website = types.KeyboardButton('google')
-# linux_bot = types.KeyboardButton('shutdown')
-# markup.add(linux_bot)
-
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
     hellper = """
     instruction
-    buttons:
-        google - открывает гугл
-        shutdown - выкл пк
+    b...
     """
 
-    bot.send_message(message.chat.id, hellper, reply_markup=markup)
+    bot.send_message(message.chat.id, hellper)
 
 
 # метод для команд (основное меню)
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['menu'])
 def menu(message): 
+
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+
     linux_bot = types.KeyboardButton('shutdown')
-
-
     markup.add(linux_bot)
-    command_os.voice()
+
+    # command_os.voice()
 
     bot.send_message(message.chat.id, 'Здравствуйте, сэр', reply_markup=markup)
 
 
-# метод для обработки текста, после кнопок
-@bot.message_handler()
+
+@bot.message_handler(content_types=['text'])
 def menu_responce(message):
 
-    # открывает гугл
-    # if message.text 
-    # google_searsh = re.finditer("загугли ", message.text)
+    
     if re.finditer("загугли ", message.text):
         search = ' '.join(message.text.split()[1:])
-        url = f"https://www.google.com/search?q={search}"  # ссылка для поиска чего либо
-        webbrowser.open(url, new=0, autoraise=True)
-        # command_os.voice()
+        if len(search) != 0:
 
-        bot.send_message(message.chat.id, f'гуглю: {search}')
+            command_google.search_google(search)
+    
+            bot.send_message(message.chat.id, f'гуглю: {search}')
 
-    # поиск по указанной ссылке
-    # open_link = re.search("https?://[\w.-]+", message.text)
+
 
     if re.search("https?://[\w.-]+", message.text):
-        url = message.text  # ссылка для поиска чего либо
-        webbrowser.open(url, new=0, autoraise=True)
+        
+        command_google.open_link(message)
+
         bot.send_message(message.chat.id, f'open: {message.text}')
+
+
 
     if message.text == "shutdown":
 
@@ -76,10 +71,14 @@ def menu_responce(message):
         markup.add(yep)
         bot.send_message(message.chat.id, 'вы уверены?', reply_markup=markup)
 
+
+
     if message.text == "yep":
         
-        bot.send_message(message.chat.id, reply_markup=markup)
-        # menu(message)
+        bot.send_message(message.chat.id, "готово")
+    
+        # command_os.voice()
+        menu(message)
         # command_os.kill_and_shutdown()
 
 
